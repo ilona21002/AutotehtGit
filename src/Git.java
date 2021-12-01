7b8d8013e1ee9b0be3d499fd69ccdf2758470a1c
ilona21002 <ilona.romppanen@student.hamk.fi>
2021-12-01 17:08:06 +0200

    Auto-tehtävän koodi

--- src/Git.java
+++ src/Git.java
@@ -2,8 +2,69 @@
 public class Git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+        
+		Auto auto1 = new Auto();
+		
+		auto1.merkki = "Citroen";
+		auto1.malli = "C4";
+		auto1.bensanMaara = 10;
+		auto1.tankkaus = 5;
+		auto1.jarruta();
+		
+		auto1.naytaTiedot();
+		
+		Auto auto2 =new Auto();
+		
+		auto2.merkki ="BMW";
+		auto2.malli = "S5";
+		auto2.bensanMaara = 5;
+		auto2.tankkaus = 10;
+		auto2.kiihdyta();
+		
+		auto2.naytaTiedot();
 
 	}
 
+} // Mainin lopetus
+
+class Auto
+{
+	public String merkki;
+	public String malli;
+	public int bensanMaara;
+	public int tankkaus;
+	
+	public void jarruta ()
+	{
+		System.out.println("Auto jarruttaa");
+	}
+
+	public void kiihdyta ()
+	{
+		System.out.println("Auto kiihtyy");
+		
+		bensanMaara = bensanMaara -1;
+		
+		if (bensanMaara<0);	
+	
+	}
+	
+	public void tankkaa()
+	{
+		
+	}
+	
+	 public void naytaTiedot ()
+	 {
+		 System.out.println("Merkki: " + merkki);
+		 System.out.println("Malli: " + malli);
+		 System.out.println("Tankissa bensaa: " + bensanMaara);
+		 System.out.println("Tankkaus: " + tankkaus);
+		 System.out.println("Tankissa bensaa tankkauksen jälkeen: " + (bensanMaara + tankkaus));
+		 
+	 }
+	
+	
 }
+
